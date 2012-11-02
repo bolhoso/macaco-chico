80a912bd9da938c6645d252380b9c236e7a95ce2
lucas <lucasrosada@gmail.com>
2012-11-02 17:31:48 -0200

    The maze is read from a file now. Adding a sample file

--- maze.c
+++ maze.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-#include <assert.h>
-
-// TODO: implement diagonal moves
-
+#include <stdlib.h>
 
 /**************************************************
  *         THE BIG AND FAT CHALLENGES             *
@@ -13,7 +10,7 @@
  *                                                *
  *************************************************/
 
-#define WALL '*'
+#define WALL '#'
 #define END  '+'
 #define PASSED '.'
 
@@ -21,55 +18,112 @@
 // they can be an increment of X or Y
 enum directions {DIR_UP, DIR_DOWN, DIR_LEFT, DIR_RIGHT};
 
-struct pos {
-	char y, x;
+typedef struct {
+	int y, x;
 	enum directions d;
-};
 
-#define MAX_LIN 8
-#define MAX_COL 16
-
-// TODO: read maze from file
-// 1 2
-// MAZE
-char maze[MAX_LIN][MAX_COL] = {
-	{'*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*'},
-	{' ', ' ', ' ', '*', ' ', ' ', '*', '*', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '+'},
-	{'*', ' ', '*', '*', '*', ' ', ' ', '*', '*', ' ', '*', ' ', '*', '*', '*', '*'},
-	{'*', ' ', '*', ' ', ' ', ' ', '*', '*', '*', ' ', '*', ' ', '*', ' ', '*', '*'},
-	{'*', ' ', '*', ' ', '*', ' ', '*', ' ', '*', ' ', '*', ' ', '*', ' ', ' ', '*'},
-	{'*', ' ', '*', ' ', '*', ' ', '*', ' ', '*', '*', '*', ' ', '*', '*', ' ', '*'},
-	{'*', ' ', ' ', ' ', '*', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '*'},
-	{'*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*'}
-};
+	int lins, cols;
+	char **maze;
+} t_maze;
 
 
-void print_maze () {
+void print_maze (t_maze *m) {
 	int i, j;
-	for (i = 0; i < MAX_LIN; i++) {
-		for (j = 0; j < MAX_COL; j++) {
-			putchar (maze[i][j]);
+
+	if (!m) {
+		return;
+	}
+
+	printf ("-> (%d, %d) %d\n", m->x, m->y, m->d);
+	for (i = 0; i < m->lins; i++) {
+		for (j = 0; j < m->cols; j++) {
+			putchar (m->maze[i][j]);
 		}
 		putchar ('\n');
 	}
 	putchar ('\n');
 }
 
+t_maze *free_maze (t_maze *m) {
+	if (!m) {
+		return;
+	}
+
+	while (--m->lins >= 0) {
+		free(m->maze[m->lins]);
+	}
+
+	free (m->maze);
+	free (m);
+	return NULL;
+}
+
+/*
+ * Read the maze from a file and initialize it
+ * The file should be in the format:
+ * <LINS> <COLS> <START_X> <START_Y>
+ * MMMMM...
+ * MMMMMM...
+ * ...
+ * MMMMM...
+ *
+ * Where M is the representation of the maze. Possible chars are:
+ *  # wall
+ *  + end of the maze (more than one allowd)
+ *  _ blank space, where it's possible to move
+ */
+t_maze *read_maze (char *file) {
+	FILE *f = fopen (file, "r");
+
+	if (!f) {
+		return NULL;
+	}
+
+	// Create a maze, read it's size and initial position
+	t_maze *my_maze = (t_maze *) malloc (sizeof (t_maze));
+	fscanf (f, "%d %d %d %d\n", &(my_maze->lins), &(my_maze->cols),
+			&(my_maze->x), &(my_maze->y));
+
+	// Allocate the maze array
+	int lin;
+	my_maze->maze = (char **) malloc (sizeof (char *) * my_maze->lins);
+	for (lin = 0; lin < my_maze->lins; lin++) {
+		my_maze->maze[lin] = (char *) malloc (sizeof (char) * (my_maze->cols + 1));
+	}
+
+	// Now, read the maze contents
+	lin = 0;
+	while (lin < my_maze->lins && !feof (f)) {
+		int read, col = 0;
+		do {
+			read = getc (f);
+
+			if (read != '\n') {
+				my_maze->maze[lin][col++] = read;
+			}
+		} while (read != '\n');
+		lin++;
+	}
+
+	fclose (f);
+	return my_maze;
+}
+
 /*
  * Check if a move is within bounds or not
  */
-int check_bounds (struct pos *p) {
+int check_bounds (t_maze *p) {
 	return !((p->x < 0) ||
-		(p->x > MAX_COL - 1) ||
+		(p->x > p->cols - 1) ||
 		(p->y < 0) ||
-		(p->y > MAX_LIN - 1));
+		(p->y > p->lins - 1));
 }
 
 /*
  * Check if it's possible to move from current (x, y) in direction d.
  * Also, check if the monkey haven't already passed in the specified position
  */
-int can_move (char maze[MAX_LIN][MAX_COL], struct pos p) {
+int can_move (t_maze p) {
 	switch (p.d) {
 		case DIR_RIGHT: p.x++; break;
 		case DIR_LEFT:  p.x--; break;
@@ -83,7 +137,7 @@ int can_move (char maze[MAX_LIN][MAX_COL], struct pos p) {
 	}
 
 	// we can move unless we have a wall and haven't passed through that pos.
-	return maze[p.y][p.x] != PASSED && maze[p.y][p.x] != WALL;
+	return p.maze[p.y][p.x] != PASSED && p.maze[p.y][p.x] != WALL;
 }
 
 /*
@@ -91,7 +145,7 @@ int can_move (char maze[MAX_LIN][MAX_COL], struct pos p) {
  * If a solution is found in the future position, returns 1, otherwise,
  * on normal moves, return 0
  */
-int move_and_reset (char maze[MAX_LIN][MAX_COL], struct pos *p) {
+int move_and_reset (t_maze *p) {
 	switch (p->d) {
 		case DIR_RIGHT:
 			p->x++; break;
@@ -103,11 +157,11 @@ int move_and_reset (char maze[MAX_LIN][MAX_COL], struct pos *p) {
 			p->y++; break;
 	}
 
-	if (maze[p->y][p->x] == END) {
+	if (p->maze[p->y][p->x] == END) {
 		return 1;
 	}
 
-	maze[p->y][p->x] = PASSED;
+	p->maze[p->y][p->x] = PASSED;
 	p->d = DIR_UP;
 
 	return 0;
@@ -115,31 +169,28 @@ int move_and_reset (char maze[MAX_LIN][MAX_COL], struct pos *p) {
 
 // TODO: nof_moves!
 #define SOLUTION_FOUND -1
-int solve_maze_recur (char maze[MAX_LIN][MAX_COL], struct pos pos) {
+int solve_maze_recur (t_maze pos) {
 
 	// Mmm.. so check the bounds
-	struct pos *p = &pos;
 	if (!check_bounds (&pos)) {
 		return 0;
 	} 
 	
-	// Change the current coordinate according to the direction and reset
-	// direction
-	if (can_move (maze, pos)) { 
-		if (move_and_reset (maze, &pos)) {
-			printf ("Hoooray! Solution found! We're at (%d, %d).\n", pos.x, pos.y);
-			return SOLUTION_FOUND;
-		}
+	if (can_move (pos)) { 
 	}
 
 	// Now, try moving in the 4 directions
 	while (pos.d <= DIR_RIGHT) {
-		if (can_move (maze, pos)) {
+		if (can_move (pos)) {
+			if (move_and_reset (&pos)) {
+				printf ("Hoooray! Solution found! We're at (%d, %d).\n", pos.x, pos.y);
+				print_maze(&pos);
+				return SOLUTION_FOUND;
+			}
 
-			printf ("-> (%d, %d) %d\n", pos.x, pos.y, pos.d);
-			print_maze();
+			print_maze(&pos);
 
-			if (solve_maze_recur (maze, pos) == SOLUTION_FOUND) {
+			if (solve_maze_recur (pos) == SOLUTION_FOUND) {
 				return SOLUTION_FOUND;
 			}
 		}
@@ -151,42 +202,14 @@ int solve_maze_recur (char maze[MAX_LIN][MAX_COL], struct pos pos) {
 	return 0;
 }
 
-int solve_maze (char maze[MAX_LIN][MAX_COL]) {
-	struct pos start = {1, 0, DIR_RIGHT};
-	return solve_maze_recur (maze, start);
-}
-
-void test_check_bounds() {
-	struct pos p1 = {0, 0, DIR_LEFT};
-	assert (!check_bounds(&p1));
-
-	struct pos p2 = {0, 0, DIR_RIGHT};
-	assert (check_bounds(&p2));
-
-	struct pos p3 = {0, 0, DIR_DOWN};
-	assert (check_bounds(&p3));
-
-	struct pos p4 = {0, 0, DIR_UP};
-	assert (!check_bounds(&p4));
-}
-
-void test_check_move () {
-	struct pos p1 = {1, 0, DIR_RIGHT};
-	assert (!can_move(maze, p1));
-}
-
-void test_solve () {
-	char simple_maze[MAX_LIN][MAX_COL] = {
-		{' ', '+'},
-	};
-
-	struct pos start = {0, 0, DIR_RIGHT};
-	solve_maze_recur (simple_maze, start);
+int solve_maze (t_maze *maze) {
+	return solve_maze_recur (*maze);
 }
 
 int main () {
-	solve_maze (maze);
-
+	t_maze *m = read_maze ("test.mz");
+	solve_maze (m);
+	m = free_maze (m);
 	
 	return 0;
 }
